78572ddc2fd9431020ebc3149c2bbdd638b39317
yyuu2479 <yyuu2479@gmail.com>
2021-07-24 04:42:17 +0000

    CARAVAN

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,30 @@
 class BlogsController < ApplicationController
+  
   def index
+    @blogs = Blog.all
   end
 
   def show
   end
 
   def new
+    @blog = Blog.new
+  end
+  
+  def create
+    list = Blog.new(blogs_params)
+    list.save
+    redirect_to blogs_path
   end
 
   def edit
   end
+  
+  
+  # プライベート
+  private
+  def blogs_params
+    params.require(:blog).permit(:title, :category, :body)
+  end
+  
 end

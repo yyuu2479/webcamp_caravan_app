26a64a72a0ff8b7ffcfe5de59c44ffcb5ff1f62d
yyuu2479 <yyuu2479@gmail.com>
2021-07-26 14:35:50 +0000

    CARAVAN

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,9 @@
 class BlogsController < ApplicationController
-  
+
   def index
     @blogs = Blog.all
+    p "標準出力にのみ反映"
+    logger.debug("標準出力とログファイルに記録される")
   end
 
   def show
@@ -11,7 +13,7 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
-  
+
   def create
     blog = Blog.new(blogs_params)
     blog.save
@@ -21,24 +23,24 @@ class BlogsController < ApplicationController
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     blog = Blog.find(params[:id])
     blog.update(blogs_params)
     redirect_to blog_path(blog)
   end
-  
+
   def destroy
     blog = Blog.find(params[:id])
     blog.destroy
     redirect_to blogs_path
   end
-  
-  
+
+
   # プライベート
   private
   def blogs_params
     params.require(:blog).permit(:title, :category, :body)
   end
-  
+
 end
